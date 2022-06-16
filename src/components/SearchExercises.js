0c546986866d0a70aa4dac586e46952c78ead06e
Aman Verma <vaman644@gmail.com>
2022-06-16 15:25:40 +0530

    fixed bugs + added similar and target exercises page

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -88,6 +88,7 @@ const SearchExercises = ({ setExercises, bodyPart, setBodyPart }) => {
           data={bodyParts}
           setBodyPart={setBodyPart}
           bodyPart={bodyPart}
+          isBodyPart
         />
       </Box>
     </Stack>
